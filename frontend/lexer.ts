2229f76cc7dd254ceb107a390aa550cc14fc8a29
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2024-02-06 00:00:11 +0100

    code

---
+++ frontend/lexer.ts
@@ -0,0 +1,100 @@
+export enum TokenType {
+    Null,
+    Number,
+    Identifier,
+    Equals,
+    OpenParen, CloseParen,
+    BinaryOperator,
+    Let,
+    EndOfFile
+}
+
+const KEYWORDS: Record<string, TokenType> = {
+    "let": TokenType.Let,
+    "null": TokenType.Null
+}
+
+export interface Token {
+    value: string,
+    type: TokenType
+}
+
+function token(value = "", type: TokenType): Token {
+    return {value, type}
+}
+
+function isAlpha(src: string) {
+    return src.toUpperCase() != src.toLowerCase();
+}
+
+function isInt(str: string) {
+    const c = str.charCodeAt(0);
+    const bounds = ['0'.charCodeAt(0), '9'.charCodeAt(0)];
+
+    return (c >= bounds[0] && c <= bounds[1]);
+}
+
+function isSkippable(str: string) {
+    return str == ' ' || str == '\n' || str == '\t';
+}
+
+export function Tokenize(sourceCode: string): Token[] {
+    const tokens = new Array<Token>();
+    const src = sourceCode.split("");
+
+    while (src.length > 0) {
+        if (src[0] == '(') {
+            tokens.push(token(src.shift(), TokenType.OpenParen));
+            continue
+        }
+        
+        if (src[0] == ")") {
+            tokens.push(token(src.shift(), TokenType.CloseParen));
+            continue
+        }
+
+        if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/" || src[0] == "%") {
+            tokens.push(token(src.shift(), TokenType.BinaryOperator));
+            continue
+        }
+
+        if (src[0] == "=") {
+            tokens.push(token(src.shift(), TokenType.Equals));
+            continue
+        }
+
+        if (isInt(src[0])) {
+            let number = "";
+            while (src.length > 0 && isInt(src[0])) {
+                number += src.shift();
+            }
+
+            tokens.push(token(number, TokenType.Number));
+            continue
+        }
+
+        if (isAlpha(src[0])) {
+            let identifier = "";
+            while (src.length > 0 && isAlpha(src[0])) {
+                identifier += src.shift();
+            }
+
+            const reserved = KEYWORDS[identifier];
+            tokens.push(token(identifier, typeof reserved == "number" ? reserved : TokenType.Identifier))
+            
+            continue
+        }
+
+        if (isSkippable(src[0])) {
+            src.shift();
+            continue
+        }
+
+        console.log('Invalid character found in source: ', src[0])
+        break;
+    }
+
+    tokens.push({type: TokenType.EndOfFile, value: "EndOfFile"})
+
+    return tokens;
+}
---
+++ frontend/parser.ts
@@ -0,0 +1,111 @@
+import type { Statement, Program, Expression, BinaryExpr, NumericLiteral, Identifier } from "./ast.ts";
+import { Tokenize, type Token, TokenType } from "./lexer.ts";
+
+export default class Parser {
+    private tokens: Token[] = []
+
+    private notEndOfFile(): boolean {
+        return this.tokens[0].type != TokenType.EndOfFile
+    }
+    
+    private at() {
+        return this.tokens[0] as Token;
+    }
+
+    private next() { // eat token
+        const previous = this.tokens.shift() as Token;
+        return previous;
+    }
+
+    private expect(type: TokenType, err: any) {
+        const prev = this.tokens.shift() as Token;
+
+        if (!prev || prev.type != type) {
+            console.log("Parser Error:\n", err, prev, "- Expecting: ", type)
+            //@ts-ignore
+            Deno.exit(1)
+        }
+    }
+
+    private parseStatement(): Statement {
+        return this.parseExpression()
+    }
+
+    private parseExpression(): Expression {
+        return this.parseAdditiveExpression();
+    }
+
+    private parseAdditiveExpression(): Expression {
+        let left = this.parseMultiplicativeExpression();
+        
+        while (this.at().value == '+' || this.at().value == "-") {
+            const operator = this.next().value;
+            const right = this.parseMultiplicativeExpression();
+    
+            left = {
+                kind: "BinaryExpr",
+                left,
+                right,
+                operator
+            } as BinaryExpr;
+        }
+
+        return left;
+    }
+    
+    private parseMultiplicativeExpression(): Expression {
+        let left = this.parsePrimaryExpression();
+        
+        while (this.at().value == '/' || this.at().value == "*" || this.at().value == "%") {
+            const operator = this.next().value;
+            const right = this.parsePrimaryExpression();
+    
+            left = {
+                kind: "BinaryExpr",
+                left,
+                right,
+                operator
+            } as BinaryExpr;
+        }
+
+        return left;
+    }
+
+    // @ts-ignore
+    private parsePrimaryExpression(): Expression {
+        const token = this.at().type;
+
+        switch (token) {
+            case TokenType.Identifier:
+                return {kind: "Identifier", symbol: this.next().value } as Identifier
+            case TokenType.Number:
+                return {kind: "NumericLiteral", value: parseFloat(this.next().value) } as NumericLiteral
+            case TokenType.OpenParen: {
+                this.next();
+                const value = this.parseExpression();
+                this.expect(TokenType.CloseParen, "Unexpected token found inside parenthesised expression. Expected closing parenthesis.")
+                return value;
+            }
+                
+            default:
+                console.error("Unexpected token found during parsing!", this.at())
+                // @ts-ignore
+                Deno.exit(1)  
+        }
+    }
+
+    public produceAST(sourceCode: string): Program {
+        this.tokens = Tokenize(sourceCode)
+
+        const program: Program = {
+            kind: "Program",
+            body: []
+        }
+
+        while (this.notEndOfFile()) {
+            program.body.push(this.parseStatement())
+        }
+
+        return program;
+    }
+}
